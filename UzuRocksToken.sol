188a02dbebcbcb5763007aa252c094718d269153
uzurocks <102502820+uzurocks@users.noreply.github.com>
2022-04-15 15:16:28 +0900

    Update UzuRocksToken.sol
    fix update

--- UzuRocksToken.sol
+++ UzuRocksToken.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
@@ -16,12 +17,6 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-interface IERC20Metadata is IERC20 {
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
-}
-
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
         return msg.sender;
@@ -32,7 +27,7 @@ abstract contract Context {
     }
 }
 
-contract ERC20 is Context, IERC20, IERC20Metadata {
+contract ERC20 is Context, IERC20 {
     mapping(address => uint256) private _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
@@ -47,15 +42,15 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         _symbol = symbol_;
     }
 
-    function name() public view virtual override returns (string memory) {
+    function name() public view virtual returns (string memory) {
         return _name;
     }
 
-    function symbol() public view virtual override returns (string memory) {
+    function symbol() public view virtual returns (string memory) {
         return _symbol;
     }
 
-    function decimals() public view virtual override returns (uint8) {
+    function decimals() public view virtual returns (uint8) {
         return 18;
     }
 
@@ -214,7 +209,9 @@ abstract contract ERC20Burnable is Context, ERC20 {
 }
 
 abstract contract Pausable is Context {
+
     event Paused(address account);
+
     event Unpaused(address account);
 
     bool private _paused;
@@ -248,80 +245,221 @@ abstract contract Pausable is Context {
     }
 }
 
-abstract contract Ownable is Context {
-    address private _owner;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+interface IAccessControl {
 
-    constructor() {
-        _transferOwnership(_msgSender());
+    event RoleAdminChanged(bytes32 indexed role, bytes32 indexed previousAdminRole, bytes32 indexed newAdminRole);
+    event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender);
+    event RoleRevoked(bytes32 indexed role, address indexed account, address indexed sender);
+    function hasRole(bytes32 role, address account) external view returns (bool);
+    function getRoleAdmin(bytes32 role) external view returns (bytes32);
+    function grantRole(bytes32 role, address account) external;
+    function revokeRole(bytes32 role, address account) external;
+    function renounceRole(bytes32 role, address account) external;
+}
+
+library Strings {
+    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+
+    function toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 
-    function owner() public view virtual returns (address) {
-        return _owner;
+    function toHexString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0x00";
+        }
+        uint256 temp = value;
+        uint256 length = 0;
+        while (temp != 0) {
+            length++;
+            temp >>= 8;
+        }
+        return toHexString(value, length);
     }
 
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
-    }
-
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _transferOwnership(newOwner);
-    }
-
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
+    function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _HEX_SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+        require(value == 0, "Strings: hex length insufficient");
+        return string(buffer);
     }
 }
 
-contract UzuRocksToken is ERC20, Pausable, Ownable, ERC20Burnable {
 
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+abstract contract ERC165 is IERC165 {
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IERC165).interfaceId;
+    }
+}
+
+abstract contract AccessControl is Context, IAccessControl, ERC165 {
+    struct RoleData {
+        mapping(address => bool) members;
+        bytes32 adminRole;
+    }
+
+    mapping(bytes32 => RoleData) private _roles;
+
+    bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
+
+    modifier onlyRole(bytes32 role) {
+        _checkRole(role, _msgSender());
+        _;
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IAccessControl).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    function hasRole(bytes32 role, address account) public view virtual override returns (bool) {
+        return _roles[role].members[account];
+    }
+
+    function _checkRole(bytes32 role, address account) internal view virtual {
+        if (!hasRole(role, account)) {
+            revert(
+                string(
+                    abi.encodePacked(
+                        "AccessControl: account ",
+                        Strings.toHexString(uint160(account), 20),
+                        " is missing role ",
+                        Strings.toHexString(uint256(role), 32)
+                    )
+                )
+            );
+        }
+    }
+
+    function getRoleAdmin(bytes32 role) public view virtual override returns (bytes32) {
+        return _roles[role].adminRole;
+    }
+
+    function grantRole(bytes32 role, address account) public virtual override onlyRole(getRoleAdmin(role)) {
+        _grantRole(role, account);
+    }
+
+    function revokeRole(bytes32 role, address account) public virtual override onlyRole(getRoleAdmin(role)) {
+        _revokeRole(role, account);
+    }
+
+    function renounceRole(bytes32 role, address account) public virtual override {
+        require(account == _msgSender(), "AccessControl: can only renounce roles for self");
+
+        _revokeRole(role, account);
+    }
+
+    function _setupRole(bytes32 role, address account) internal virtual {
+        _grantRole(role, account);
+    }
+
+    function _setRoleAdmin(bytes32 role, bytes32 adminRole) internal virtual {
+        bytes32 previousAdminRole = getRoleAdmin(role);
+        _roles[role].adminRole = adminRole;
+        emit RoleAdminChanged(role, previousAdminRole, adminRole);
+    }
+
+    function _grantRole(bytes32 role, address account) internal virtual {
+        if (!hasRole(role, account)) {
+            _roles[role].members[account] = true;
+            emit RoleGranted(role, account, _msgSender());
+        }
+    }
+
+    function _revokeRole(bytes32 role, address account) internal virtual {
+        if (hasRole(role, account)) {
+            _roles[role].members[account] = false;
+            emit RoleRevoked(role, account, _msgSender());
+        }
+    }
+}
+
+contract UzuRocksToken is ERC20, Pausable, ERC20Burnable, AccessControl {
+
+    // Roles
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE"); 
+    bytes32 public constant LOCKER_ROLE = keccak256("LOCKER_ROLE"); 
+
+    // lock
     mapping (address => uint256) private _lockBalance;
 
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
+    // Declare an Event
+    event SetLock(address indexed _account, uint256 _amount);
+    event UnLock(address indexed _account, uint256 _amount);
+
+    // init
+    constructor(address pauser, address locker) ERC20("UzuRocksToken", "UZRS") {
+       _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+       _setupRole(PAUSER_ROLE, pauser);
+       _setupRole(LOCKER_ROLE, locker);
+
+       _mint(msg.sender, 2500000000 * 10 ** decimals());
     }
 
     function getAccountLock(address account) public view returns (uint256) {
         return _lockBalance[account];
     }
 
-    function setLock(address account, uint256 amount) onlyOwner public returns (bool) {
+    function setLock(address account, uint256 amount) onlyRole(LOCKER_ROLE) public returns (bool) {
 
         require((balanceOf(account) - getAccountLock(account)) >= amount, "ERC20: lock amount is exceeded");
 
         _lockBalance[account] += amount; 
+
+        emit SetLock(account, amount);
         return true;
     }
 
-    function multiLock(address[] memory accounts, uint256[] memory amounts) onlyOwner public returns (bool) {
+    function multiLock(address[] memory accounts, uint256[] memory amounts) onlyRole(LOCKER_ROLE) public returns (bool) {
+
+        require(accounts.length == amounts.length, "UzuRocksToken: lengths of two arrays are not equal");
+
         for(uint i = 0; i < accounts.length; i++) {
             setLock(accounts[i], amounts[i]);
         }
         return true;
     }
 
-    function unlock(address account, uint256 amount) onlyOwner public returns (bool) {
+    function unlock(address account, uint256 amount) onlyRole(LOCKER_ROLE) public returns (bool) {
 
-        require(amount <= getAccountLock(account), "ERC20: unlock amount is exceeded.");
+        require(getAccountLock(account) <= amount, "ERC20: unlock amount is exceeded.");
 
         _lockBalance[account] -= amount;
+        emit UnLock(account, amount);
         return true;
     }
 
-    function pause() public onlyOwner {
+    function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
@@ -334,6 +472,9 @@ contract UzuRocksToken is ERC20, Pausable, Ownable, ERC20Burnable {
     }
 
     function multiTransfer(address[] memory accounts, uint256[] memory amounts) public returns (bool) {
+
+        require(accounts.length == amounts.length, "UzuRocksToken: lengths of two arrays are not equal");
+
         for(uint i = 0; i < accounts.length; i++) {
             _transfer(msg.sender, accounts[i], amounts[i]);
         }
@@ -341,6 +482,7 @@ contract UzuRocksToken is ERC20, Pausable, Ownable, ERC20Burnable {
     }
 
     function _transfer(address sender, address recipient, uint256 amount) internal override {
+
         require((balanceOf(sender) - getAccountLock(sender)) >= amount, "ERC20: transfer amount exceeds balance");
 	    return super._transfer(sender, recipient, amount);
 	}
